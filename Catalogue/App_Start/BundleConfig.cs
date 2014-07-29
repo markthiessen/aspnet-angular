2c1de9f3e66017434c1a4994a515ce934ce58462
Mark Thiessen <mark.thiessen@live.com>
2014-07-28 21:08:43 -0600

    Adds AngularJS scripts and bundle

--- Catalogue/App_Start/BundleConfig.cs
+++ Catalogue/App_Start/BundleConfig.cs
@@ -20,6 +20,10 @@ namespace Catalogue
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-route.js", "~/Scripts/angular-resource.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
